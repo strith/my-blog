8cf205525ba6942621c7ee6ab32a3b0d7c99ca06
Michael Aston <michaelastonjr@gmail.com>
2019-12-14 15:54:52 -0600

    First commit

---
+++ src/build/precache-manifest.d48b6a7a04129b70277a3b7302e8bfdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7541e21fb858f3fbe1496696c8d26f3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f0c61ba3bb0026d2874",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "bef9bfdc3204b7500250",
+    "url": "/static/js/2.217e16b1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.217e16b1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f0c61ba3bb0026d2874",
+    "url": "/static/js/main.fad1aa33.chunk.js"
+  },
+  {
+    "revision": "2829fc8889d8b91e8c4b",
+    "url": "/static/js/runtime-main.b519a9b9.js"
+  }
+]);
